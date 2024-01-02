ac2d95f6986a4d35ff34f2dc15eb322fc392eca2
4th-yeolhoonyoon <yoonyeolhoon@4thparty.co.kr>
2024-01-02 11:00:26 +0900

    test commit

---
+++ graph.py
@@ -0,0 +1,10 @@
+graph1 = {1: [2, 3, 5], 2: [1, 3], 3: [1, 2, 4], 4: [3, 5], 5: [1,4]}
+graph2 = {1: [2, 3], 2: [3], 3: [4], 4: [], 5: [1, 4]}
+
+
+def dfs_recursive(graph, node):
+    res = []
+    visited = set()
+    pass
+
+
